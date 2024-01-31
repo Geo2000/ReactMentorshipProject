f4ce4fec6cae650f09720f956f77ecfd5e1e9a53
Georgiana Murarasu <georgiana.murarasu@tss-yonder.com>
2024-01-31 19:46:52 +0200

    Create app + dummy components

---
+++ react-mentorship-project/src/App.tsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Profile from "./pages/Profile";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Movies from "./pages/Movies";
+import NoPage from "./pages/NoPage";
+
+export default function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Movies />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="*" element={<NoPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
---
+++ react-mentorship-project/src/pages/Login.tsx
@@ -0,0 +1,7 @@
+export default function Login() {
+  return (
+    <>
+      <h2>This is Login</h2>
+    </>
+  );
+}
