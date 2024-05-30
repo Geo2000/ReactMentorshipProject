866f9a42beee8fbc3297c2de90ca27c2d70cb289
Geo2000 <georgianamurarasu@gmail.com>
2024-05-30 12:53:37 +0300

    Add filtering by title name

--- react-mentorship-project/src/components/MovieList.tsx
+++ react-mentorship-project/src/components/MovieList.tsx
@@ -1,11 +1,14 @@
 import { useState, useEffect } from "react";
+import { TextField } from "@mui/material";
 
 import MovieCard from "./MovieCard";
 import Movie from "../types/Movie";
 import { API_KEY } from "../constants";
+import "../styles/MovieList.css";
 
 const MovieList = () => {
   const [movies, setMovies] = useState<Movie[]>([]);
+  const [searchQuery, setSearchQuery] = useState("");
 
   useEffect(() => {
     fetch(`https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}`)
@@ -16,9 +19,36 @@ const MovieList = () => {
       .catch((error) => console.error("Error fetching movies: ", error));
   }, []);
 
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const filteredFilms =
+    Array.isArray(movies) &&
+    movies.length &&
+    movies.filter(
+      (movie: any) =>
+        movie.title &&
+        movie.title.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+
   return (
     <>
-      {Array.isArray(movies) && movies.length && (
+      <div className="search-input">
+        <TextField
+          label="Search Movies"
+          variant="outlined"
+          value={searchQuery}
+          onChange={handleSearch}
+          sx={{
+            input: {
+              background: "white",
+              borderRadius: '10px',
+            }
+          }}
+        />
+      </div>
+      {Array.isArray(filteredFilms) && filteredFilms.length && (
         <div
           style={{
             display: "flex",
@@ -26,7 +56,7 @@ const MovieList = () => {
             justifyContent: "space-around",
           }}
         >
-          {movies.map((movie) => (
+          {filteredFilms.map((movie) => (
             <MovieCard key={movie.id} movie={movie} />
           ))}
         </div>
--- react-mentorship-project/src/pages/Movies.tsx
+++ react-mentorship-project/src/pages/Movies.tsx
@@ -2,16 +2,17 @@ import MovieList from "../components/MovieList";
 import Navbar from "../components/Navbar";
 import background from "../assets/img/background.avif";
 
-export default function Movies({handleLogout}: any) {
+export default function Movies({ handleLogout }: any) {
   return (
     <>
       <div
         style={{
           backgroundImage: `url(${background})`,
           backgroundSize: "cover",
+          height: "100vh",
         }}
       >
-        <Navbar handleLogout={handleLogout}/>
+        <Navbar handleLogout={handleLogout} />
         <MovieList />
       </div>
     </>
